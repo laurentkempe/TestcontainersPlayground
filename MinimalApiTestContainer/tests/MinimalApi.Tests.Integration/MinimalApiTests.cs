03d4795960012ce4bd8d2cea65e4f1e6864e55a9
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2023-07-01 10:21:24 +0200

    Add integration tests for MinimalApi project
    An integration test has been added for the MinimalApi project. This test
    starts a Docker container with the application's image and checks the
    response from the weather forecast endpoint. Essential packages
    necessary for integration tests were added in the
    MinimalApi.Tests.Integration project. Also, the MinimalApi project now
    supports running in a Docker container as specified in the csproj file.
    Minor adjustments were made in launchSettings.json to reflect these
    changes.

--- MinimalApiTestContainer/tests/MinimalApi.Tests.Integration/MinimalApiTests.cs
+++ MinimalApiTestContainer/tests/MinimalApi.Tests.Integration/MinimalApiTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using DotNet.Testcontainers.Builders;
+using NUnit.Framework;
 
 namespace MinimalApi.Tests.Integration;
 
@@ -6,8 +7,36 @@ namespace MinimalApi.Tests.Integration;
 public class MinimalApiTests
 {
     [Test]
-    public void METHOD()
+    public async Task Call_WeatherForecast_ExpectResponse()
     {
-        
+        // Create a new instance of a container.
+        var container = new ContainerBuilder()
+            // Set the image for the container to "laurentkempe/minimalapi:1.0.0".
+            .WithImage("laurentkempe/minimalapi:1.0.0")
+            // Bind port 80 of the container to a random port on the host.
+            .WithPortBinding(80, true)
+            // Wait until the HTTP endpoint of the container is available.
+            .WithWaitStrategy(Wait.ForUnixContainer().UntilHttpRequestIsSucceeded(r => r.ForPath("/weatherforecast")))
+            // Build the container configuration.
+            .Build();
+
+        // Start the container.
+        await container.StartAsync()
+            .ConfigureAwait(false);
+
+        // Create a new instance of HttpClient to send HTTP requests.
+        var httpClient = new HttpClient();
+
+        // Construct the request URI by specifying the scheme, hostname, assigned random host port, and the endpoint "uuid".
+        var requestUri =
+            new UriBuilder(Uri.UriSchemeHttp, container.Hostname, container.GetMappedPublicPort(80), "weatherforecast")
+                .Uri;
+
+        // Send an HTTP GET request to the specified URI and retrieve the response as a string.
+        var response = await httpClient.GetStringAsync(requestUri)
+            .ConfigureAwait(false);
+
+        // Ensure that the retrieved UUID is a valid GUID.
+        Assert.That(response, Is.Not.Empty);
     }
 }
