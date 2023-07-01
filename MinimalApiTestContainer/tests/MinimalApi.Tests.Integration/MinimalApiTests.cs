d0c623570ad7fc653eeff1ad0746bc20892d6d26
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2023-07-01 09:35:05 +0200

    Initial creation of TestcontainersPlayground with a 1st empty project

---
+++ MinimalApiTestContainer/tests/MinimalApi.Tests.Integration/MinimalApiTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace MinimalApi.Tests.Integration;
+
+[TestFixture]
+public class MinimalApiTests
+{
+    [Test]
+    public void METHOD()
+    {
+        
+    }
+}
